f04ba1be0b214c6b1fd03f1c60e9ffc3aa219c3a
Rook Soto <rook@Vault-Tec.home>
2017-01-07 02:20:03 -0500

    Added Conditionals++

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -8,6 +8,14 @@ public class Conditionals {
         System.out.println(isOddAndIsMultipleOfThree2(9));
         fizzMultipleofThree(3);
 
+        // Extra
+        System.out.println(cigarParty(100, true));
+        System.out.println(caughtSpeeding(65, true));
+        System.out.println(alarmClock(0, false));
+        System.out.println(lotteryTicket(1, 1, 1));
+        System.out.println(blackjack(19, 23));
+        System.out.println(evenlySpaced(5, 5, 5));
+
     }
 
     // Wrote this one in the first exercise,
@@ -36,4 +44,58 @@ public class Conditionals {
             System.out.println(num);
         }
     }
+
+    private static boolean cigarParty(int cigars, boolean isWeekend) {
+        if (cigars >= 40) {
+            if (cigars <= 60 || isWeekend) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int caughtSpeeding(int speed, boolean isBirthday) {
+        if (isBirthday) speed -= 5;
+        if (speed >= 81) return 2;
+        if (speed > 60) return 1;
+        return 0;
+    }
+
+    private static String alarmClock(int day, boolean vacation) {
+        if (day > 0 && day < 6) {
+            if (vacation) return "10:00";
+            return "7:00";
+        }
+        if (day == 0 || day == 6) {
+            if (vacation) return "off";
+            return "10:00";
+        }
+        return "off";
+    }
+
+    private static int lotteryTicket(int a, int b, int c) {
+        if (a == b && b == c) return 20;
+        if (a == b || b == c || c == a) return 10;
+        return 0;
+    }
+
+    private static int blackjack(int a, int b) {
+        if (a > 21 && b <= 21) return b;
+        if (b > 21 && a <= 21) return a;
+        if (a <= 21 && b <= 21) {
+            if ((21 - b) < (21 - a)) return b;
+            if ((21 - a) < (21 - b)) return a;
+        }
+        return 0;
+    }
+
+    private static boolean evenlySpaced(int a, int b, int c) {
+        if (a == b && b == c) return true;
+        if (a == b || b == c || c == a) return false;
+
+        return Math.abs(a - b) == Math.abs(b - c) ||
+                Math.abs(b - c) == Math.abs(c - a) ||
+                Math.abs(c - a) == Math.abs(a - b);
+    }
+
 }
--- src/Loops.java
+++ src/Loops.java
@@ -1,9 +1,6 @@
-/**
- * Created by alexandraqin on 4/14/15.
- */
 public class Loops {
 
-  public static void main (String args[]) {
+    public static void main(String args[]) {
 
-  }
+    }
 }
